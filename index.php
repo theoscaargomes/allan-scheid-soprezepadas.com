4a7fbe7cd3864b7262e3d51564aaca507e9913f9
Oscar Gomes <medinagomes.oscar7@gmail.com>
2020-12-11 09:53:27 -0300

    newupdate
    files

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
     <title>soprezepadas.com/allan-scheid</title>
 <body>
 
-<?php
-echo "Precisou, é só chamar!!";
-?>
-
-<img src="IMG_20190724_125434046.jpg" alt="AVS" style="width:500px;height:600px;">
+<h1>Preciso, é só chamar!!</h1>
+<img src="IMG_20190724_125434046.jpg" alt="AVS" style="width:300px;height:500px;">
 
 </body>
 </html>
