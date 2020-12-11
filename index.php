e80976fd74b21e2752fd94afe6455f94bfc3af32
Oscar Gomes <medinagomes.oscar7@gmail.com>
2020-12-11 09:49:01 -0300

    commit
    New update

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <title>soprezepadas.com/allan-scheid</title>
+<body>
+
+<?php
+echo "Precisou, é só chamar!!";
+?>
+
+<?php 
+    echo "<img src='IMG_20190724_125434046.jpg' alt='Allan-scheid>"; 
+?> 
+
+</body>
+</html>
